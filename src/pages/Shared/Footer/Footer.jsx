efddd845b786615b886b4fb462805c72cc35b454
Talha <mdtalha2008@gmail.com>
2024-12-09 00:13:33 +0600

    font family add

--- src/pages/Shared/Footer/Footer.jsx
+++ src/pages/Shared/Footer/Footer.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Footer = () => {
     return (
         <div>
-           <div className="bg-gray-100 py-8 px-4">
+           <div className="bg-gray-100 py-8 px-4" style={{ fontFamily: "Ubuntu, sans-serif", fontWeight: 400 }}>
             <div className="max-w-7xl mx-auto grid grid-cols-1 md:grid-cols-2 lg:grid-cols-6 gap-8">
                 {/* Today's Kakao */}
                 <div>
