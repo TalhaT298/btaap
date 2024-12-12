adf92ecb689a22e48b13bd9a2e5fe6bd64e04332
talha <mdtalha2008@gmail.com>
2024-12-13 01:46:00 +0600

    moving add

--- src/pages/Home/Home/Home.jsx
+++ src/pages/Home/Home/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import Banner from '../Banner/Banner';
 import Group from '../Group/Group';
+import Moving from '../Moving/Moving';
 
 const Home = () => {
     return (
         <div>
             <Banner></Banner>
             <Group></Group>
+            <Moving></Moving>
         </div>
     );
 };
---
+++ src/pages/Home/Moving/Moving.jsx
@@ -0,0 +1,21 @@
+// import React from 'react';
+
+// const Moving = () => {
+//   return (
+//     <div>
+
+//     </div>
+//   );
+// };
+// export default Moving;
+import React from 'react';
+
+const Moving = () => {
+  return (
+    <div className='pt-10'>
+        ggggg
+        jjj
+    </div>
+  );
+};
+export default Moving;
