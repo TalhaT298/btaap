448f89cd0892bc5267c0bc06bb6b8adeb34b36ac
talha <mdtalha2008@gmail.com>
2024-12-15 02:16:09 +0600

    news add at home.jsx

--- src/pages/Home/News/News.jsx
+++ src/pages/Home/News/News.jsx
@@ -3,7 +3,27 @@ import React from 'react'
 const News = () => {
   return (
     <div>
-      
+       {/* title add */}
+            <div className="flex items-center justify-between py-4">
+              <div className="flex items-center space-x-2">
+                <div
+                  className="w-16 h-12
+                       flex items-center justify-center rounded-lg "
+                >
+                  <div className=" ">
+                    {" "}
+                    <img className="rounded-xl " src={bulb} alt="" />{" "}
+                  </div>
+                </div>
+                <span
+                  className="text-5xl font-extrabold text-black px-6"
+                  style={{ fontFamily: "Ubuntu, sans-serif", fontWeight: 500 }}
+                >
+                  Btaap News
+                </span>
+              </div>
+            </div>
+            {/* title end */}
     </div>
   )
 }
