986ae831d4a5cd6e0e6483e741cf1d5c621147f0
talha <mdtalha2008@gmail.com>
2024-12-15 01:15:03 +0600

    world add

--- src/pages/Home/World/World.jsx
+++ src/pages/Home/World/World.jsx
@@ -7,6 +7,9 @@ import plus from "../../../../src/assets/plus.svg";
 import ev from "../../../../src/assets/evaly.svg";
 import dr from "../../../../src/assets/draz.svg";
 import am from "../../../../src/assets/amazone.svg";
+import map from "../../../../src/assets/map.svg";
+import qa from "../../../../src/assets/qatar.svg";
+import car from "../../../../src/assets/car.svg";
 const World = () => {
   return (
     <div className="py-8">
@@ -55,9 +58,9 @@ const World = () => {
         <div class="bg-gray-100 p-4 rounded-xl shadow flex h-36 pt-6">
           <h2 class="font-extrabold text-4xl pt-6" style={{ fontFamily: "Ubuntu, sans-serif", fontWeight: 600 }}>Life</h2>
           <div class="flex pt-2 px-4">
-            <div class="p-1 rounded-full"><img className="rounded-2xl" src={btap} alt="" /></div>
-            <div class="p-1 rounded-full"><img className="rounded-2xl" src={facebook} alt="" /></div>
-            <div class="p-1 rounded-full"><img className="rounded-2xl" src={insta} alt="" /></div>
+            <div class="p-1 rounded-full"><img className="rounded-2xl" src={map} alt="" /></div>
+            <div class="p-1 rounded-full"><img className="rounded-2xl" src={qa} alt="" /></div>
+            <div class="p-1 rounded-full"><img className="rounded-2xl" src={car} alt="" /></div>
             <div class="p-1 rounded-full"><img className="rounded-2xl" src={plus} alt="" /></div>
           </div>
         </div>
