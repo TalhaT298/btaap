bad897e397751156a0475bc12ba1dcc00b3c716b
talha <mdtalha2008@gmail.com>
2024-12-16 00:07:56 +0600

    title add at home

--- src/pages/Home/Help/Help.jsx
+++ src/pages/Home/Help/Help.jsx
@@ -1,9 +1,29 @@
 import React from 'react'
-
+import promise from "../../../../src/assets/promise.gif";
 const Help = () => {
   return (
-    <div>
-      
+    <div className='py-8'>
+        {/* title add */}
+      <div className="flex items-center justify-between py-8">
+              <div className="flex items-center space-x-2">
+                <div
+                  className="w-16 h-16
+                                   flex items-center justify-center rounded-lg "
+                >
+                  <div className=" ">
+                    {" "}
+                    <img className="rounded-xl " src={promise} alt="" />{" "}
+                  </div>
+                </div>
+                <span
+                  className="text-5xl font-extrabold text-black px-6"
+                  style={{ fontFamily: "Ubuntu, sans-serif", fontWeight: 500 }}
+                >
+                  Promise and Responsibility
+                </span>
+              </div>
+            </div>
+            {/* title end */}
     </div>
   )
 }
