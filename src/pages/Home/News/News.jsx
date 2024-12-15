affdcbc3b9cf019d9d3d329a5722d5dd0e1ea58b
talha <mdtalha2008@gmail.com>
2024-12-15 19:44:36 +0600

    2 divs add

--- src/pages/Home/News/News.jsx
+++ src/pages/Home/News/News.jsx
@@ -159,7 +159,25 @@ const News = () => {
         </div>
 
         {/* Card 3 */}
-        
+        <div class="bg-gray-100 p-4 rounded-xl shadow flex h-36 pt-6">
+                  <h2 class="font-extrabold text-4xl pt-6" style={{ fontFamily: "Ubuntu, sans-serif", fontWeight: 600 }}>Communication</h2>
+                  <div class="flex pt-2 px-4">
+                    {/* <div class="p-1 rounded-full"><img className="rounded-2xl" src={btap} alt="" /></div>
+                    <div class="p-1 rounded-full"><img className="rounded-2xl" src={facebook} alt="" /></div>
+                    <div class="p-1 rounded-full"><img className="rounded-2xl" src={insta} alt="" /></div>
+                    <div class="p-1 rounded-full"><img className="rounded-2xl" src={plus} alt="" /></div> */}
+                  </div>
+                </div>
+
+                <div class="bg-gray-100 p-4 rounded-xl shadow flex h-36 pt-6">
+                          <h2 class="font-extrabold text-4xl pt-6" style={{ fontFamily: "Ubuntu, sans-serif", fontWeight: 600 }}>Communication</h2>
+                          <div class="flex pt-2 px-4">
+                            {/* <div class="p-1 rounded-full"><img className="rounded-2xl" src={btap} alt="" /></div>
+                            <div class="p-1 rounded-full"><img className="rounded-2xl" src={facebook} alt="" /></div>
+                            <div class="p-1 rounded-full"><img className="rounded-2xl" src={insta} alt="" /></div>
+                            <div class="p-1 rounded-full"><img className="rounded-2xl" src={plus} alt="" /></div> */}
+                          </div>
+                        </div>
       </div>
     </div>
   );
