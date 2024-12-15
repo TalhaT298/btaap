1982ec7c1500f1d5e694368aced069df71b81082
talha <mdtalha2008@gmail.com>
2024-12-15 12:18:59 +0600

    news add

--- src/pages/Home/News/News.jsx
+++ src/pages/Home/News/News.jsx
@@ -1,31 +1,31 @@
-import React from 'react'
-
+import React from "react";
+import bulb from "../../../../src/assets/bulb.png";
 const News = () => {
   return (
     <div>
-       {/* title add */}
-            <div className="flex items-center justify-between py-4">
-              <div className="flex items-center space-x-2">
-                <div
-                  className="w-16 h-12
+      {/* title add */}
+      <div className="flex items-center justify-between py-4">
+        <div className="flex items-center space-x-2">
+          <div
+            className="w-16 h-12
                        flex items-center justify-center rounded-lg "
-                >
-                  <div className=" ">
-                    {" "}
-                    <img className="rounded-xl " src={bulb} alt="" />{" "}
-                  </div>
-                </div>
-                <span
-                  className="text-5xl font-extrabold text-black px-6"
-                  style={{ fontFamily: "Ubuntu, sans-serif", fontWeight: 500 }}
-                >
-                  Btaap News
-                </span>
-              </div>
+          >
+            <div className=" ">
+              {" "}
+              <img className="rounded-xl " src={bulb} alt="" />{" "}
             </div>
-            {/* title end */}
+          </div>
+          <span
+            className="text-5xl font-extrabold text-black px-6"
+            style={{ fontFamily: "Ubuntu, sans-serif", fontWeight: 500 }}
+          >
+            Btaap News
+          </span>
+        </div>
+      </div>
+      {/* title end */}
     </div>
-  )
-}
+  );
+};
 
-export default News
+export default News;
