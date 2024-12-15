73d28cebd92bb983b75f74266e67720f72cb8ecb
talha <mdtalha2008@gmail.com>
2024-12-15 12:34:52 +0600

    card add

--- src/pages/Home/News/News.jsx
+++ src/pages/Home/News/News.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import bulb from "../../../../src/assets/bulb.png";
+import calender from "../../../../src/assets/calender.gif";
 const News = () => {
   return (
     <div>
@@ -7,23 +7,63 @@ const News = () => {
       <div className="flex items-center justify-between py-4">
         <div className="flex items-center space-x-2">
           <div
-            className="w-16 h-12
+            className="w-16 h-16
                        flex items-center justify-center rounded-lg "
           >
             <div className=" ">
               {" "}
-              <img className="rounded-xl " src={bulb} alt="" />{" "}
+              <img className="rounded-xl " src={calender} alt="" />{" "}
             </div>
           </div>
           <span
             className="text-5xl font-extrabold text-black px-6"
             style={{ fontFamily: "Ubuntu, sans-serif", fontWeight: 500 }}
           >
-            Btaap News
+            Today's News
           </span>
         </div>
       </div>
       {/* title end */}
+      <div className="flex flex-wrap justify-center gap-4">
+        <div className="card bg-base-100 w-96 shadow-xl">
+          <div className="card-body">
+            <h2 className="card-title">Shoes!</h2>
+            <p>If a dog chews shoes whose shoes does he choose?</p>
+          </div>
+          <figure>
+            <img
+              src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp"
+              alt="Shoes"
+            />
+          </figure>
+        </div>
+
+        <div className="card bg-base-100 w-96 shadow-xl">
+          <div className="card-body">
+            <h2 className="card-title">Shoes!</h2>
+            <p>If a dog chews shoes whose shoes does he choose?</p>
+          </div>
+          <figure>
+            <img
+              src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp"
+              alt="Shoes"
+            />
+          </figure>
+        </div>
+
+        <div className="card bg-base-100 w-96 shadow-xl">
+          <div className="card-body">
+            <h2 className="card-title">Shoes!</h2>
+            <p>If a dog chews shoes whose shoes does he choose?</p>
+          </div>
+          <figure>
+            <img
+              src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp"
+              alt="Shoes"
+            />
+          </figure>
+        </div>
+      </div>
     </div>
   );
 };
