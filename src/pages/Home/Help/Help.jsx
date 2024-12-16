b3920e56ac4d6ba8f286a3e0bf35f0a96e1bc476
talha <mdtalha2008@gmail.com>
2024-12-16 22:01:51 +0600

    short cut implement perfectly

--- src/pages/Home/Help/Help.jsx
+++ src/pages/Home/Help/Help.jsx
@@ -4,7 +4,7 @@ import ba1 from "../../../../src/assets/ba1.png";
 import ba2 from "../../../../src/assets/ba2.png";
 import map3 from "../../../../src/assets/map.webp";
 import robot2 from "../../../../src/assets/robot2.webp";
-import { AiTwotoneSound } from "react-icons/ai";
+import { FaArrowCircleRight } from "react-icons/fa";
 import { AiOutlineCheckCircle } from "react-icons/ai";
 import { IoLogoInstagram } from "react-icons/io5";
 import { FaFacebook } from "react-icons/fa";
@@ -38,39 +38,74 @@ const Help = () => {
 
       {/* section add  */}
       <div className="grid grid-cols-1 lg:grid-cols-3 gap-4 justify-center p-4">
-              {/* Card 1 */}
-              <div className="max-w-md mx-auto bg-gray-100 rounded-2xl shadow-md overflow-hidden">
-                <div className="p-4">
-                  <div className="flex items-center space-x-2">
-                    <div className="flex items-center text-[#97ce9b]">
-                      <AiTwotoneSound className="text-[#97ce9b] w-8 h-8" />
-                    </div>
-                    <span className="text-sm font-extralight bg-white text-black px-5 py-2 rounded-lg">
-                      Press release
-                    </span>
-                    <span className="text-sm text-gray-500">2024.12.13</span>
-                    <span className="text-xl px-2">⫶</span>
-                  </div>
-                  <h2 className="text-4xl font-semibold mt-2 py-10" style={{ fontFamily: "Ubuntu, sans-serif", fontWeight: 500 }}>
-                    Btaap to hold '2024 Kakao Business Best Awards' ceremony
-                  </h2>
-                  <div className="mt-24 flex flex-wrap gap-2 text-sm text-gray-600">
-                    <span>#Symbiosis</span>
-                    <span>#Btaap</span>
-                    <span>#BtaapBusiness</span>
+        {/* Card 1 */}
+        <div>
+          <div class="flex flex-col space-y-6   min-h-screen">
+            {/* <!-- Card 1 --> */}
+            <div class="flex items-center justify-between p-4 h-40 bg-gray-100 shadow-md rounded-2xl ">
+              <div>
+                <h2 class="text-3xl font-semibold text-black"  style={{ fontFamily: "Ubuntu, sans-serif", fontWeight: 500 }}>
+                  Btaap Customer Cen
+                </h2>
+                <p class="text-lg text-gray-600 flex items-center pt-4">
+                  Shortcut
+                  <span class="text-black ">
+                  <FaArrowCircleRight className="w-6 h-6 ml-4"/>
+                  </span>
+                </p>
+              </div>
+              <div class="w-14 h-14 rounded-full bg-yellow-400 flex items-center justify-center">
+                <span class="font-bold text-black">TALK</span>
+              </div>
+            </div>
+
+            {/* <!-- Card 2 --> */}
+            <div class="flex items-center justify-between p-4 h-40 bg-gray-100 rounded-2xl shadow-md">
+              <div>
+                <h2 class="text-3xl font-semibold text-black"  style={{ fontFamily: "Ubuntu, sans-serif", fontWeight: 500 }}>
+                  Next Customer Cent
+                </h2>
+                <p class="text-lg text-gray-600 flex items-center pt-4">
+                  Shortcut
+                  <span class="text-black ">
+                  <FaArrowCircleRight className="w-6 h-6 ml-4"/>
+                  </span>
+                </p>
+              </div>
+              <div class="w-14 h-14 rounded-full border-4 border-red-500 flex items-center justify-center">
+                <span class="font-bold text-red-500">cafe</span>
+              </div>
+            </div>
+
+            {/* <!-- Card 3 --> */}
+            <div class="flex items-center justify-between p-4 h-40 bg-gray-100 rounded-2xl shadow-md">
+              <div>
+                <h2 class="text-3xl font-semibold text-black"  style={{ fontFamily: "Ubuntu, sans-serif", fontWeight: 500 }}>
+                  Commerce Custome Center
+                </h2>
+                <p class="text-lg text-gray-600 flex items-center pt-4">
+                  Shortcut
+                  <span class="text-black ">
+                  <FaArrowCircleRight className="w-6 h-6 ml-4"/>
+                  </span>
+                </p>
+              </div>
+              <div class="w-14 h-14 rounded-full bg-yellow-400 flex items-center justify-center">
+                <div class="relative">
+                  <div class="w-6 h-6 bg-white border border-black rounded"></div>
+                  <div class="absolute top-[-8px] left-1/2 -translate-x-1/2 text-red-500">
+                    ❤️
                   </div>
                 </div>
-                <img
-                  src={ba1}
-                  alt="Btaap Business Best Awards"
-                  className="w-full h-56 object-cover"
-                />
               </div>
-      
-              {/* Card 2 */}
-              <div className="max-w-md mx-auto bg-[#97ce9b] rounded-2xl shadow-md overflow-hidden">
-                <div className="p-4">
-                  {/* <div className="flex items-center space-x-2">
+            </div>
+          </div>
+        </div>
+
+        {/* Card 2 */}
+        <div className="max-w-md mx-auto bg-[#97ce9b] rounded-2xl shadow-md overflow-hidden">
+          <div className="p-4">
+            {/* <div className="flex items-center space-x-2">
                     <div className="flex items-center text-[#97ce9b]">
                       <AiTwotoneSound className="text-[#97ce9b] w-8 h-8" />
                     </div>
@@ -80,39 +115,41 @@ const Help = () => {
                     <span className="text-sm text-gray-500">2024.12.12</span>
                     <span className="text-xl px-2">⫶</span>
                   </div> */}
-                  <h2 className="text-4xl text-white font-semibold mt-2 pt-10" style={{ fontFamily: "Ubuntu, sans-serif", fontWeight: 500 }}>
-                  Subscribe to various news from Btaap!
-                  </h2>
-                  <div className="mt-12 flex flex-wrap gap-2 text-xl text-white">
-                    {/* <span>#ESG</span>
+            <h2
+              className="text-4xl text-white font-semibold mt-2 pt-10"
+              style={{ fontFamily: "Ubuntu, sans-serif", fontWeight: 500 }}
+            >
+              Subscribe to various news from Btaap!
+            </h2>
+            <div className="mt-12 flex flex-wrap gap-2 text-xl text-white">
+              {/* <span>#ESG</span>
                     <span>#TogetherValues</span>
                     <span>#Btaap</span> */}
-                    <p>Go to official social media</p>
-                   
-                  </div>
-                  <div className="mt-12 flex flex-wrap gap-2 justify-center items-center">
-                    {/* <span>#ESG</span>
+              <p>Go to official social media</p>
+            </div>
+            <div className="mt-12 flex flex-wrap gap-2 justify-center items-center">
+              {/* <span>#ESG</span>
                     <span>#TogetherValues</span>
                     <span>#Btaap</span> */}
-                   
-                    <IoLogoInstagram className=" w-10 h-10 text-sm text-white"/>
-                    <FaFacebook className=" w-10 h-10 text-sm text-white"/>
-                    <FaLinkedinIn className=" w-10 h-10 text-sm text-white"/>
-                    <FaDiscord className=" w-10 h-10 text-sm text-white"/>
-                    <FaYoutube className=" w-10 h-10 text-sm text-white"/>
-                  </div>
-                </div>
-                <img
-                  src={robot2}
-                  alt="Kakao Together Value"
-                  className="w-full h-56 object-cover"
-                />
-              </div>
-      
-              {/* Card 3 */}
-              <div className="max-w-md mx-auto bg-[#5a461b] rounded-2xl shadow-md overflow-hidden">
-                <div className="p-4">
-                  {/* <div className="flex items-center space-x-2">
+
+              <IoLogoInstagram className=" w-10 h-10 text-sm text-white" />
+              <FaFacebook className=" w-10 h-10 text-sm text-white" />
+              <FaLinkedinIn className=" w-10 h-10 text-sm text-white" />
+              <FaDiscord className=" w-10 h-10 text-sm text-white" />
+              <FaYoutube className=" w-10 h-10 text-sm text-white" />
+            </div>
+          </div>
+          <img
+            src={robot2}
+            alt="Kakao Together Value"
+            className="w-full h-56 object-cover"
+          />
+        </div>
+
+        {/* Card 3 */}
+        <div className="max-w-md mx-auto bg-[#5a461b] rounded-2xl shadow-md overflow-hidden">
+          <div className="p-4">
+            {/* <div className="flex items-center space-x-2">
                     <div className="flex items-center text-[#97ce9b]">
                       <AiTwotoneSound className="text-[#97ce9b] w-8 h-8" />
                     </div>
@@ -122,21 +159,20 @@ const Help = () => {
                     <span className="text-sm text-gray-500">2024.12.11</span>
                     <span className="text-xl px-2">⫶</span>
                   </div> */}
-                  <h2 className="text-4xl text-white font-semibold mt-2 pt-10" style={{ fontFamily: "Ubuntu, sans-serif", fontWeight: 500 }}>
-                  If you are interested in everything in the world,
-                  you are already a member of the Btaap crew.
-                  </h2>
-                  <div className="mt-[56px] flex flex-wrap gap-2 text-xl text-white">
-                  Recruitment news <AiOutlineCheckCircle className="w-7 h-7"/>
-                  </div>
-                </div>
-                <img
-                  src={map3}
-                  alt=""
-                  className="w-full h-56 object-cover"
-                />
-              </div>
+            <h2
+              className="text-4xl text-white font-semibold mt-2 pt-10"
+              style={{ fontFamily: "Ubuntu, sans-serif", fontWeight: 500 }}
+            >
+              If you are interested in everything in the world, you are already
+              a member of the Btaap crew.
+            </h2>
+            <div className="mt-[56px] flex flex-wrap gap-2 text-xl text-white">
+              Recruitment news <AiOutlineCheckCircle className="w-7 h-7" />
             </div>
+          </div>
+          <img src={map3} alt="" className="w-full h-56 object-cover" />
+        </div>
+      </div>
     </div>
   );
 };
