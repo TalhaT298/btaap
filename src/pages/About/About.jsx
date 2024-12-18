8147af8a700b2f37528d41269f387f9ec17aac4b
talha <mdtalha2008@gmail.com>
2024-12-18 21:20:41 +0600

    btaap scroll add

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -4,6 +4,8 @@ import { FaFacebook } from "react-icons/fa";
 import { FaLinkedinIn } from "react-icons/fa6";
 import { FaDiscord } from "react-icons/fa";
 import { FaYoutube } from "react-icons/fa";
+import abou1 from "../../../src/assets/about1.webp";
+import "./About.css";
 const About = () => {
   return (
     <div>
@@ -52,7 +54,23 @@ const About = () => {
       </div>
       {/* another section */}
       <div>
-        
+        <div className="relative h-screen overflow-hidden">
+          {/* Scrolling Image */}
+          <div
+            className="absolute inset-0 h-full overflow-y-scroll scrollbar-thin scrollbar-thumb-gray-600 scrollbar-track-gray-300"
+            style={{
+              msOverflowStyle: "auto", // Ensure scrollbars are visible in Internet Explorer
+              scrollbarWidth: "thin", // Make the scrollbar thinner in Firefox
+            }}
+          >
+            <img src={abou1} alt="Scrolling Image" className="w-full" />
+          </div>
+
+          {/* Centered Text */}
+          <div className="absolute inset-0 flex items-center justify-center pointer-events-none">
+            <p className="text-white text-5xl font-bold px-4 py-2">Btaap</p>
+          </div>
+        </div>
       </div>
     </div>
   );
