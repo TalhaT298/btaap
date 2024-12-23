557997b4bb03ec825243ba29b8c793c5b1e79185
talha <mdtalha2008@gmail.com>
2024-12-24 00:23:32 +0600

    banner responsive done

--- src/pages/Home/Banner/Banner.jsx
+++ src/pages/Home/Banner/Banner.jsx
@@ -10,34 +10,34 @@ const Banner = () => {
   const images = [img1, img2, img3]; // Array of images for the slider
   const [currentImageIndex, setCurrentImageIndex] = useState(0);
 
-  // Automatically change the image eery 3 seconds
+  // Automatically change the image every 3 seconds
   useEffect(() => {
     const interval = setInterval(() => {
       setCurrentImageIndex((prevIndex) => (prevIndex + 1) % images.length);
     }, 3000);
 
-    return () => clearInterval(interval); // Cleanup on component unmoun
+    return () => clearInterval(interval); // Cleanup on component unmount
   }, [images.length]);
 
   return (
     <React.Fragment>
-      <div className="flex w-full">
+      <div className="flex flex-col lg:flex-row w-full">
         {/* Left Side (80%) */}
-        <div className="w-4/5 relative">
-          <h2 className="text-white text-5xl font-extrabold absolute top-6 left-6 p-4" style={{ fontFamily: "Ubuntu, sans-serif", fontWeight: 400}}>
+        <div className="lg:w-4/5 relative w-full">
+          <h2 className="text-white text-3xl sm:text-4xl lg:text-5xl font-extrabold absolute top-6 left-6 p-4" style={{ fontFamily: "Ubuntu, sans-serif", fontWeight: 400 }}>
             Digital Literacy for Seniors <br />Closer with Btaap
           </h2>
-          <img src={banner} alt="" />
+          <img src={banner} alt="" className="w-full h-auto" />
         </div>
 
         {/* Right Side (20%) */}
-        <div className="w-1/5 flex flex-col space-y-4 p-4">
+        <div className="lg:w-1/5 flex flex-col space-y-4 p-4 w-full">
           {/* First Box */}
           <div className="bg-gray-600 flex items-center justify-center h-48 rounded-lg shadow-md">
             <img
               className="h-full w-full object-cover rounded-lg"
               src={calender}
-              alt=""
+              alt="Calendar"
             />
           </div>
 
@@ -55,7 +55,7 @@ const Banner = () => {
             <img
               className="h-full w-full object-cover rounded-lg"
               src={world}
-              alt=""
+              alt="World"
             />
           </div>
         </div>
@@ -65,6 +65,7 @@ const Banner = () => {
 };
 
 export default Banner;
+
 // import React from 'react'
 
 // const Banner = () => {
