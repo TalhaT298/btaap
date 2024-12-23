531d80817a3dc93c9141649626fd6bfe4d702b60
talha <mdtalha2008@gmail.com>
2024-12-24 00:56:31 +0600

    respon done fore responsibility

--- src/pages/Home/Responsibility/Responsibility.jsx
+++ src/pages/Home/Responsibility/Responsibility.jsx
@@ -4,23 +4,18 @@ import sc from "../../../../src/assets/sc.gif";
 import tree from "../../../../src/assets/tree.gif";
 import coin from "../../../../src/assets/coin.gif";
 import { IoArrowForwardCircleSharp } from "react-icons/io5";
+
 const Responsibility = () => {
   return (
     <div className="py-8">
-      {/* title add */}
+      {/* title */}
       <div className="flex items-center justify-between py-8">
         <div className="flex items-center space-x-2">
-          <div
-            className="w-16 h-16
-                             flex items-center justify-center rounded-lg "
-          >
-            <div className=" ">
-              {" "}
-              <img className="rounded-xl " src={promise} alt="" />{" "}
-            </div>
+          <div className="w-16 h-16 flex items-center justify-center rounded-lg">
+            <img className="rounded-xl" src={promise} alt="" />
           </div>
           <span
-            className="text-5xl font-extrabold text-black px-6"
+            className="text-3xl sm:text-4xl lg:text-5xl font-extrabold text-black px-4 sm:px-6"
             style={{ fontFamily: "Ubuntu, sans-serif", fontWeight: 500 }}
           >
             Promise and Responsibility
@@ -28,18 +23,32 @@ const Responsibility = () => {
         </div>
       </div>
       {/* title end */}
-      <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
-        <div class="relative">
-          <img src={sc} alt="Image 1" class="w-full h-auto rounded-3xl" />
-          <IoArrowForwardCircleSharp className="absolute top-[500px] right-4 transform -translate-y-1/2 w-16 h-16 text-black" />
+
+      {/* Images grid */}
+      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
+        <div className="relative">
+          <img
+            src={sc}
+            alt="Image 1"
+            className="w-full h-auto rounded-3xl"
+          />
+          <IoArrowForwardCircleSharp className="absolute top-[80%] right-4 transform -translate-y-1/2 w-12 h-12 sm:w-16 sm:h-16 text-black" />
         </div>
-        <div class="relative">
-          <img src={tree} alt="Image 2" class="w-full h-auto rounded-3xl"  />
-          <IoArrowForwardCircleSharp className="absolute top-[500px] right-4 transform -translate-y-1/2 w-16 h-16 text-black" />
+        <div className="relative">
+          <img
+            src={tree}
+            alt="Image 2"
+            className="w-full h-auto rounded-3xl"
+          />
+          <IoArrowForwardCircleSharp className="absolute top-[80%] right-4 transform -translate-y-1/2 w-12 h-12 sm:w-16 sm:h-16 text-black" />
         </div>
-        <div class="relative">
-          <img src={coin} alt="Image 3" class="w-full h-auto rounded-3xl" />
-          <IoArrowForwardCircleSharp className="absolute top-[500px] right-4 transform -translate-y-1/2 w-16 h-16 text-black" />
+        <div className="relative">
+          <img
+            src={coin}
+            alt="Image 3"
+            className="w-full h-auto rounded-3xl"
+          />
+          <IoArrowForwardCircleSharp className="absolute top-[80%] right-4 transform -translate-y-1/2 w-12 h-12 sm:w-16 sm:h-16 text-black" />
         </div>
       </div>
     </div>
