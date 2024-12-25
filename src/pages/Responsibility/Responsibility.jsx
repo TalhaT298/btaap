8559df8a9b6cb06c6a52f58ab37678bdf9b19951
talha <mdtalha2008@gmail.com>
2024-12-25 17:47:20 +0600

    first section add at responsibility

--- src/pages/Responsibility/Responsibility.jsx
+++ src/pages/Responsibility/Responsibility.jsx
@@ -1,15 +1,24 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React from "react";
+import { Link } from "react-router-dom";
 
 const Responsibility = () => {
   return (
     <div>
       <Link to="/responsibility"></Link>
-      <div>
+      {/* 1st section start */}
+      <div className="navbar bg-base-100">
+        <div className="navbar-start">
+          
+          <a className="  text-xl">ESG Reports</a>
+        </div>
         
+        <div className="navbar-end">
+          <a className="btn">Button</a>
+        </div>
       </div>
+      {/* 1st section finish */}
     </div>
-  )
-}
+  );
+};
 
-export default Responsibility
+export default Responsibility;
