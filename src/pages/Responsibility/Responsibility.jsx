64dccc8e976c65c2a41525b2fae61e63e39d3577
talha <mdtalha2008@gmail.com>
2024-12-25 15:40:20 +0600

    responsibility add at link

--- src/pages/Responsibility/Responsibility.jsx
+++ src/pages/Responsibility/Responsibility.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Responsibility = () => {
   return (
     <div>
-      
+      <Link to="/responsibility"></Link>
+      <div>
+        
+      </div>
     </div>
   )
 }
