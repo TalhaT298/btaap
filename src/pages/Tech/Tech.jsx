58ddd365cb913377a3b573bb2793380c62faed2d
talha <mdtalha2008@gmail.com>
2024-12-25 13:31:35 +0600

    scroll top add in teach

--- src/pages/Tech/Tech.jsx
+++ src/pages/Tech/Tech.jsx
@@ -1,37 +1,50 @@
-import React from 'react'
+import React from "react";
 import ba1 from "../../../src/assets/ba1.png";
 import site from "../../../src/assets/site.jpg";
 import look from "../../../src/assets/look.jpg";
 import { AiTwotoneSound } from "react-icons/ai";
 import setting from "../../../src/assets/setting.gif";
-import { SettingOutlined } from '@ant-design/icons';
+import { SettingOutlined } from "@ant-design/icons";
+import { IoArrowUpCircleSharp } from "react-icons/io5";
+import { Link } from "react-router-dom";
 const Tech = () => {
+  const scrollToTop = () => {
+    window.scrollTo({
+        top: 0,
+        behavior: 'smooth',
+    });
+}
   return (
-    <div>
+    <div className="pt-16">
       {/* title add */}
-            <div className="flex items-center justify-between py-6 pl-6">
-              <div className="flex items-center space-x-2">
-                <div
-                  className="w-16 h-16
+      <div className="flex items-center justify-between py-6 pl-6">
+        <div className="flex items-center space-x-2">
+          <div
+            className="w-16 h-16
                              flex items-center justify-center rounded-lg "
-                >
-                  <div className=" ">
-                    {" "}
-                    <img className="rounded-xl " src={setting} alt="" />{" "}
-                  </div>
-                </div>
-                <span
-                  className="text-5xl font-extrabold text-black px-6"
-                  style={{ fontFamily: "Ubuntu, sans-serif", fontWeight: 500 }}
-                >
-                  Tech
-                </span>
-              </div>
+          >
+            <div className=" ">
+              {" "}
+              <img className="rounded-xl " src={setting} alt="" />{" "}
             </div>
-            {/* title end */}
-            <div className='py-4 pl-4'>
-              <p className='text-3xl'style={{ fontFamily: "Ubuntu, sans-serif", fontWeight: 400 }} >Btaap's Technology, Lowering the barrier to the future</p>
-            </div>
+          </div>
+          <span
+            className="text-5xl font-extrabold text-black px-6"
+            style={{ fontFamily: "Ubuntu, sans-serif", fontWeight: 500 }}
+          >
+            Tech
+          </span>
+        </div>
+      </div>
+      {/* title end */}
+      <div className="py-4 pl-4">
+        <p
+          className="text-3xl"
+          style={{ fontFamily: "Ubuntu, sans-serif", fontWeight: 400 }}
+        >
+          Btaap's Technology, Lowering the barrier to the future
+        </p>
+      </div>
       {/* card start */}
       <div className="grid grid-cols-1 lg:grid-cols-3 gap-4 justify-center p-4">
         {/* Card 1 */}
@@ -39,23 +52,28 @@ const Tech = () => {
           <div className="p-4">
             <div className="flex items-center space-x-2">
               <div className="flex items-center text-[#97ce9b]">
-                <SettingOutlined  className="text-[#1e40af] " style={{ fontSize: '24px', height: '24px', width: '24px' }}  />
+                <SettingOutlined
+                  className="text-[#1e40af] "
+                  style={{ fontSize: "24px", height: "24px", width: "24px" }}
+                />
               </div>
               <span className="text-sm font-extralight bg-white text-black px-5 py-2 rounded-lg">
-              Btaap Tech
+                Btaap Tech
               </span>
-               
+
               <span className="text-xl px-2">⫶</span>
             </div>
             <h2
               className="text-3xl sm:text-4xl font-semibold mt-2 py-10"
               style={{ fontFamily: "Ubuntu, sans-serif", fontWeight: 500 }}
             >
-              Introducing Btaap's Al <br />model, Kanana Model <br />Family
+              Introducing Btaap's Al <br />
+              model, Kanana Model <br />
+              Family
             </h2>
             <div className="mt-4 flex flex-wrap gap-2 text-sm text-gray-600">
-            <span>#AI</span>
-            <span>#ifBtaap</span>
+              <span>#AI</span>
+              <span>#ifBtaap</span>
               <span>#kanan</span>
               <span>#AllRecomend</span>
             </div>
@@ -72,12 +90,15 @@ const Tech = () => {
           <div className="p-4">
             <div className="flex items-center space-x-2">
               <div className="flex items-center text-[#97ce9b]">
-              <SettingOutlined  className="text-[#1e40af] " style={{ fontSize: '24px', height: '24px', width: '24px' }}  />
+                <SettingOutlined
+                  className="text-[#1e40af] "
+                  style={{ fontSize: "24px", height: "24px", width: "24px" }}
+                />
               </div>
               <span className="text-sm font-extralight bg-white text-black px-5 py-2 rounded-lg">
-              Btaap Tech
+                Btaap Tech
               </span>
-              
+
               <span className="text-xl px-2">⫶</span>
             </div>
             <h2
@@ -87,8 +108,7 @@ const Tech = () => {
               Accessibility of Btaap <br /> Corporate Sites
             </h2>
             <div className="mt-24 flex flex-wrap gap-2 text-sm text-gray-600">
-            <span>#WebAccessibility</span>
-              
+              <span>#WebAccessibility</span>
             </div>
           </div>
           <img
@@ -103,22 +123,26 @@ const Tech = () => {
           <div className="p-4">
             <div className="flex items-center space-x-2">
               <div className="flex items-center text-[#97ce9b]">
-              <SettingOutlined  className="text-[#1e40af] " style={{ fontSize: '24px', height: '24px', width: '24px' }}  />
+                <SettingOutlined
+                  className="text-[#1e40af] "
+                  style={{ fontSize: "24px", height: "24px", width: "24px" }}
+                />
               </div>
               <span className="text-sm font-extralight bg-white text-black px-5 py-2 rounded-lg">
-              Btaap Tech
+                Btaap Tech
               </span>
-               
+
               <span className="text-xl px-2">⫶</span>
             </div>
             <h2
               className="text-3xl sm:text-4xl font-semibold mt-2 pt-10"
               style={{ fontFamily: "Ubuntu, sans-serif", fontWeight: 500 }}
             >
-              Lowering the barrier to <br />the future 
+              Lowering the barrier to <br />
+              the future
             </h2>
             <div className="mt-24 flex flex-wrap gap-2 text-sm text-gray-600">
-            <span>#developer</span>
+              <span>#developer</span>
               <span>#engineering</span>
               <span>#tech</span>
             </div>
@@ -131,8 +155,18 @@ const Tech = () => {
         </div>
       </div>
       {/* card end */}
+      <div className="flex justify-end items-end py-10">
+              <div
+                onClick={scrollToTop}
+                className="list-none "
+              >
+                <Link to="">
+                  <IoArrowUpCircleSharp className="w-16 h-16 " />
+                </Link>
+              </div>
+            </div>
     </div>
-  )
-}
+  );
+};
 
-export default Tech
+export default Tech;
