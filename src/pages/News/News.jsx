1374815033641d3ae38f3f513e28fd40743e95ba
talha <mdtalha2008@gmail.com>
2024-12-27 01:36:35 +0600

    Link add at news

--- src/pages/News/News.jsx
+++ src/pages/News/News.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const News = () => {
   return (
     <div>
-      
+      <Link to="/news"></Link>
+      <div>
+        
+      </div>
     </div>
   )
 }
