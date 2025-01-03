aba777ba09865177ce256968d52507a3e10c70c9
talha <mdtalha2008@gmail.com>
2025-01-04 01:43:19 +0600

    link add at communication

--- src/pages/Communication/Communication.jsx
+++ src/pages/Communication/Communication.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Communication = () => {
   return (
     <div>
-      
+      <Link to="/communication"></Link>
     </div>
   )
 }
