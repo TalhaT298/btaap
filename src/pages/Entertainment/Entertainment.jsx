199fa9d4a5a73ffdb04de89f5c4a1083bc4a3b44
talha <mdtalha2008@gmail.com>
2025-01-06 14:22:52 +0600

    link add at entertainment

--- src/pages/Entertainment/Entertainment.jsx
+++ src/pages/Entertainment/Entertainment.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Entertainment = () => {
   return (
     <div>
-      
+      <Link to="/entertainment"></Link>
     </div>
   )
 }
