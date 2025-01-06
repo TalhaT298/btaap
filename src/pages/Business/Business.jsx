62084dd6f5cb9949e29cfc4ae85f91798fc6c35c
talha <mdtalha2008@gmail.com>
2025-01-06 14:20:15 +0600

    busniess add

--- src/pages/Business/Business.jsx
+++ src/pages/Business/Business.jsx
@@ -1,12 +1,16 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React from "react";
+import { Helmet } from "react-helmet-async";
+import { Link } from "react-router-dom";
 
 const Business = () => {
   return (
     <div>
+      <Helmet>
+        <title>Btaap | Business</title>
+      </Helmet>
       <Link to="/business"></Link>
     </div>
-  )
-}
+  );
+};
 
-export default Business
+export default Business;
