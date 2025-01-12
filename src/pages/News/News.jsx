0aa4ff22e21cb20dd612027a6b2dea18639eeb01
talha <mdtalha2008@gmail.com>
2025-01-12 16:35:41 +0600

    news update

--- src/pages/News/News.jsx
+++ src/pages/News/News.jsx
@@ -9,7 +9,26 @@ const News = () => {
       <Helmet>
         <title>Btaap | News</title>
       </Helmet>
-      <div></div>
+       {/* title add */}
+            <div className="flex items-center justify-between py-6 pl-6">
+              <div className="flex items-center space-x-2">
+                <div
+                  className="w-16 h-16
+                                         flex items-center justify-center rounded-lg "
+                >
+                  <div className=" ">
+                    {" "}
+                    {/* <img className="rounded-xl " src={bulbc} alt="" />{" "} */}
+                  </div>
+                </div>
+                <span
+                  className="text-5xl font-extrabold text-black px-6"
+                  style={{ fontFamily: "Ubuntu, sans-serif", fontWeight: 500 }}
+                >
+                  Service
+                </span>
+              </div>
+            </div>
     </div>
   );
 };
