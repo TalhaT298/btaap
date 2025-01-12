aea439c15984fb49141493fd8bf3dfb22853f5da
talha <mdtalha2008@gmail.com>
2025-01-12 16:24:59 +0600

    helmet add at news

--- src/pages/News/News.jsx
+++ src/pages/News/News.jsx
@@ -1,15 +1,17 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React from "react";
+import { Helmet } from "react-helmet-async";
+import { Link } from "react-router-dom";
 
 const News = () => {
   return (
     <div>
       <Link to="/news"></Link>
-      <div>
-        
-      </div>
+      <Helmet>
+        <title>Btaap | News</title>
+      </Helmet>
+      <div></div>
     </div>
-  )
-}
+  );
+};
 
-export default News
+export default News;
