2d890af7b6fe85eedaeb0a7c8e148f8058e1bf48
talha <mdtalha2008@gmail.com>
2025-03-18 16:03:43 +0600

    Link and helmet add at library

--- src/pages/Libray/Libray.jsx
+++ src/pages/Libray/Libray.jsx
@@ -1,16 +1,16 @@
-import React from 'react'
-import { Helmet } from 'react-helmet-async'
-import { Link } from 'react-router-dom'
+import React from "react";
+import { Helmet } from "react-helmet-async";
+import { Link } from "react-router-dom";
 
 const Libray = () => {
   return (
     <div>
       <Link to="/libray"></Link>
-            <Helmet>
-              <title>Btaap | Library</title>
-            </Helmet>
+      <Helmet>
+        <title>Btaap | Library</title>
+      </Helmet>
     </div>
-  )
-}
+  );
+};
 
-export default Libray
+export default Libray;
