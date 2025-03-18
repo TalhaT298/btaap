3bbc31beaebd600fb01c1d303f1e617d2f88924a
talha <mdtalha2008@gmail.com>
2025-03-18 16:03:02 +0600

    Libray add

--- src/pages/Libray/Libray.jsx
+++ src/pages/Libray/Libray.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
+import { Helmet } from 'react-helmet-async'
+import { Link } from 'react-router-dom'
 
 const Libray = () => {
   return (
     <div>
-      
+      <Link to="/libray"></Link>
+            <Helmet>
+              <title>Btaap | Library</title>
+            </Helmet>
     </div>
   )
 }
