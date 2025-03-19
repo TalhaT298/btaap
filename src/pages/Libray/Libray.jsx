27b9a4832b9b40ee72e92b9cfb59ecba766ad844
talha <mdtalha2008@gmail.com>
2025-03-20 00:46:58 +0600

    book add

--- src/pages/Libray/Libray.jsx
+++ src/pages/Libray/Libray.jsx
@@ -25,7 +25,7 @@ const Libray = () => {
           </div>
 
           {/* <!-- Book Grid --> */}
-          <div class="grid grid-cols-3 gap-6">
+          <div class="grid grid-cols-4 gap-6">
             {/* <!-- Book Card --> */}
             <div class="bg-white p-4 rounded-lg shadow-lg text-center">
               <div class="h-32 w-24 bg-gradient-to-r from-red-500 to-blue-500 mx-auto rounded-lg"></div>
@@ -43,6 +43,36 @@ const Libray = () => {
               <div class="h-32 w-24 bg-gradient-to-r from-green-500 to-red-500 mx-auto rounded-lg"></div>
               <p class="mt-2 font-semibold">The Good Muslim</p>
               <p class="text-gray-500 text-sm">Tahmima Anam, 2011</p>
+            </div>
+
+            <div class="bg-white p-4 rounded-lg shadow-lg text-center">
+              <div class="h-32 w-24 bg-gradient-to-r from-red-500 to-blue-500 mx-auto rounded-lg"></div>
+              <p class="mt-2 font-semibold">Book Title</p>
+              <p class="text-gray-500 text-sm">Author, Year</p>
+            </div>
+
+            <div class="bg-white p-4 rounded-lg shadow-lg text-center">
+              <div class="h-32 w-24 bg-gradient-to-r from-red-500 to-blue-500 mx-auto rounded-lg"></div>
+              <p class="mt-2 font-semibold">Book Title</p>
+              <p class="text-gray-500 text-sm">Author, Year</p>
+            </div>
+
+            <div class="bg-white p-4 rounded-lg shadow-lg text-center">
+              <div class="h-32 w-24 bg-gradient-to-r from-red-500 to-blue-500 mx-auto rounded-lg"></div>
+              <p class="mt-2 font-semibold">Book Title</p>
+              <p class="text-gray-500 text-sm">Author, Year</p>
+            </div>
+
+            <div class="bg-white p-4 rounded-lg shadow-lg text-center">
+              <div class="h-32 w-24 bg-gradient-to-r from-red-500 to-blue-500 mx-auto rounded-lg"></div>
+              <p class="mt-2 font-semibold">Book Title</p>
+              <p class="text-gray-500 text-sm">Author, Year</p>
+            </div>
+
+            <div class="bg-white p-4 rounded-lg shadow-lg text-center">
+              <div class="h-32 w-24 bg-gradient-to-r from-red-500 to-blue-500 mx-auto rounded-lg"></div>
+              <p class="mt-2 font-semibold">Book Title</p>
+              <p class="text-gray-500 text-sm">Author, Year</p>
             </div>
           </div>
         </div>
