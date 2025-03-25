1bec00252a4edead232ecfa4a7b40297f7dc537d
talha <mdtalha2008@gmail.com>
2025-03-26 00:06:33 +0600

    classic add at libk

--- src/pages/Classic/Classic.jsx
+++ src/pages/Classic/Classic.jsx
@@ -1,16 +1,19 @@
-import React from 'react'
-import { Helmet } from 'react-helmet-async'
-import { Link } from 'react-router-dom'
+import React from "react";
+import { Helmet } from "react-helmet-async";
+import { Link } from "react-router-dom";
 
 const Classic = () => {
   return (
     <div>
       <Link to="/classic"></Link>
-            <Helmet>
-              <title>Btaap | Class</title>
-            </Helmet>
+      <Helmet>
+        <title>Btaap | Class</title>
+      </Helmet>
+      <div>
+        
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Classic
+export default Classic;
