575ae82ff67c40fd72160c9b55e6f22144590d8e
talha <mdtalha2008@gmail.com>
2025-03-26 00:04:31 +0600

    classic add

---
+++ src/pages/Classic/Classic.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import { Helmet } from 'react-helmet-async'
+import { Link } from 'react-router-dom'
+
+const Classic = () => {
+  return (
+    <div>
+      <Link to="/classic"></Link>
+            <Helmet>
+              <title>Btaap | Class</title>
+            </Helmet>
+    </div>
+  )
+}
+
+export default Classic
