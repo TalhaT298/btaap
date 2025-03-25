7dc9ad611adc3511037c428d7a504e5f2d515d46
talha <mdtalha2008@gmail.com>
2025-03-26 00:22:23 +0600

    link work perfectly

--- src/pages/Classic/Classic.jsx
+++ src/pages/Classic/Classic.jsx
@@ -1,8 +1,14 @@
-import React from "react";
+import React, { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
 import { Helmet } from "react-helmet-async";
-import { Link } from "react-router-dom";
+
 
 const Classic = () => {
+    const [input, setInput] = useState("");
+    const navigate = useNavigate();
+    const submitHandler = () => {
+      navigate(`/room/${input}`);
+    };
   return (
     <div>
       <Link to="/classic"></Link>
@@ -10,7 +16,19 @@ const Classic = () => {
         <title>Btaap | Class</title>
       </Helmet>
       <div>
-        
+      <input
+        value={input}
+        onChange={(e) => setInput(e.target.value)}
+        type="text"
+        placeholder="Enter your name"
+        className="w-full p-2 border   rounded"
+      />
+      <button
+        className="btn mt-2 w-full md:w-auto bg-sky-600 text-white hover:text-black rounded"
+        onClick={submitHandler}
+      >
+        Join
+      </button>
       </div>
     </div>
   );
